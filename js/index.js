4ef8b23af197595ef65e50802d8c581cdedd9a46
Jeremy Blake <jeremy.j.blake44@gmail.com>
2024-04-13 13:33:35 +0200

    collision blocks drawn

--- js/index.js
+++ js/index.js
@@ -9,51 +9,32 @@ const scaledCanvas = {
     height: canvas.height / 4
 }
 
+const floorCollisions2D = []
+for(let i = 0; i < floorCollisions.length; i += 36) {
+    floorCollisions2D.push(floorCollisions.slice(i, i + 36))
+
+}
+
+const collisionBlocks = []
+floorCollisions2D.forEach((row, y) => {
+    row.forEach((symbol, x) => {
+        if(symbol === 202) {
+            console.log('draw a block here!')
+            collisionBlocks.push(
+                new CollisionBlock({
+                    position: {
+                        x: x * 16,
+                        y: y * 16
+                    }
+                })
+            )
+        }
+    })
+})
+
+console.log(collisionBlocks)
+
 const gravity = 0.1
-
-class Sprite {
-  constructor({position, imageSrc}) {
-    this.position = position
-    this.image = new Image()
-    this.image.src = imageSrc
-  }
-
-  draw() {
-   if (!this.image) return
-    c.drawImage(this.image, this.position.x, this.position.y)  
-  }
-
-  update() {
-    this.draw()
-  }
-    
-}
-
-class Player {
-    constructor(position) {
-        this.position = position
-        this.velocity = {
-            x: 0,
-            y: 1
-        };
-        this.height = 100
-    }
-    
-    draw() {
-        c.fillStyle = 'blue'
-        c.fillRect(this.position.x, this.position.y, 100, this.height)
-    }
-    
-    update() {
-        this.draw()
-
-        this.position.x += this.velocity.x;
-        this.position.y += this.velocity.y;
-        this.velocity.y += gravity;
-        if (this.position.y + this.height + this.velocity.y < canvas.height) this.velocity.y += gravity;
-        else this.velocity.y = 0
-    }
-}
 
 const player = new Player({
     x: 0,
@@ -90,6 +71,9 @@ function animate() {
     c.scale(4, 4);
     c.translate(0, -background.image.height + scaledCanvas.height);
     background.update();
+    collisionBlocks.forEach((collisionBlock) => {
+      collisionBlock.update()
+    })
     c.restore();
 
     player.update();
@@ -127,5 +111,3 @@ window.addEventListener('keyup', (event) => {
        
     }
 })
-
-console.log(floorCollisions)
